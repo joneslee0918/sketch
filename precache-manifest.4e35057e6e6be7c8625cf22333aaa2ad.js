bd7860fe69dd8d3f9f1181be1962ebcbe8a6d49a
joneslee0918 <joneslee0918@gmail.com>
2023-04-02 08:29:58 +0300

    first commit

---
+++ precache-manifest.4e35057e6e6be7c8625cf22333aaa2ad.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = [
+  {
+    "revision": "576c84dbdaa74c8760a2",
+    "url": "/static/css/main.8d6994c5.chunk.css"
+  },
+  {
+    "revision": "576c84dbdaa74c8760a2",
+    "url": "/static/js/main.ca27d3bb.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "c669658a7b8d2dec4973",
+    "url": "/static/css/2.dd7d5fad.chunk.css"
+  },
+  {
+    "revision": "c669658a7b8d2dec4973",
+    "url": "/static/js/2.6fd6951b.chunk.js"
+  },
+  {
+    "revision": "0ec155f6822bdac9334117f3f2394bb5",
+    "url": "/static/media/no_image.0ec155f6.jpg"
+  },
+  {
+    "revision": "507b5c7cc9fbc33d6b505f39d8fae494",
+    "url": "/static/media/background.507b5c7c.png"
+  },
+  {
+    "revision": "183f2e8487677396f1440b7f3094f366",
+    "url": "/static/media/1.183f2e84.png"
+  },
+  {
+    "revision": "e8edfd307e15558a261d6a0a84f145c2",
+    "url": "/static/media/1_over.e8edfd30.png"
+  },
+  {
+    "revision": "d3deed9abada7e4ae1b15abe97a9d5dd",
+    "url": "/static/media/7.d3deed9a.png"
+  },
+  {
+    "revision": "116115fdcef653b0aa20e84a285341e7",
+    "url": "/static/media/7_over.116115fd.png"
+  },
+  {
+    "revision": "d974b271c591de6f13761056f29586b6",
+    "url": "/static/media/5.d974b271.png"
+  },
+  {
+    "revision": "ecbc6fd20da8c77b68fae5333551ab8e",
+    "url": "/static/media/5_over.ecbc6fd2.png"
+  },
+  {
+    "revision": "3934f6be4bc100ed7da3b98dd782853b",
+    "url": "/static/media/3.3934f6be.png"
+  },
+  {
+    "revision": "e066c4017514b190c0d24bdd6b5f0d73",
+    "url": "/static/media/3_over.e066c401.png"
+  },
+  {
+    "revision": "411203d7d1b7f9b5d67fb2bbe5a936f6",
+    "url": "/static/media/2.411203d7.png"
+  },
+  {
+    "revision": "1716e3d9b891adf5590606931f92e542",
+    "url": "/static/media/2_over.1716e3d9.png"
+  },
+  {
+    "revision": "5f0b653ada618480f110c5c939b622d6",
+    "url": "/static/media/4.5f0b653a.png"
+  },
+  {
+    "revision": "451a46401d2f6554f760ea45d6044344",
+    "url": "/static/media/4_over.451a4640.png"
+  },
+  {
+    "revision": "38fbb3482f6bb8e5dba825ff884f057d",
+    "url": "/static/media/6.38fbb348.png"
+  },
+  {
+    "revision": "62613895d730fee6ea66e17a234d2e2a",
+    "url": "/static/media/6_over.62613895.png"
+  },
+  {
+    "revision": "be58c1a42ca694e736b8cc8bec8af369",
+    "url": "/static/media/8.be58c1a4.png"
+  },
+  {
+    "revision": "9bdb9e8502f9acb375fcc35cd2d3f0de",
+    "url": "/static/media/8_over.9bdb9e85.png"
+  },
+  {
+    "revision": "19669c6f31247375f8756f68fc80c5d1",
+    "url": "/static/media/Garnet-Capitals-Bold.19669c6f.otf"
+  },
+  {
+    "revision": "ff085ae7a07b2c5c335dc763e3113c65",
+    "url": "/index.html"
+  }
+];
